0f353e520258c1da332edc9ae4a579461472cea2
Greg Wallace <greg@gregtwallace.com>
2017-05-05 16:07:41 -0400

    g3-common: add ims

--- g3.mk
+++ g3.mk
@@ -110,6 +110,23 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Gello
 
+# ims
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enabled_by_hw=1 \
+    DEVICE_PROVISIONED=1 \
+    ril.subscription.types=NV,RUIM
+
 # IPv6 tethering
 PRODUCT_PACKAGES += \
     ebtables \
@@ -189,6 +206,7 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.baseband.sh \
     init.sensors.sh \
+    init.qti.ims.sh \
     ueventd.g3.rc
 
 # Thermal
