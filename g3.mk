feed34a4cf605e3a4a8e077030e01be16b801227
Greg Wallace <greg@gregtwallace.com>
2017-05-06 11:27:14 -0400

    g3-common: Disable VT

--- g3.mk
+++ g3.mk
@@ -119,13 +119,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.csvt.enabled=false \
     persist.radio.jbims=1 \
     persist.radio.ROTATION_ENABLE=1 \
-    persist.radio.VT_ENABLE=1 \
-    persist.radio.VT_HYBRID_ENABLE=1 \
-    persist.radio.VT_USE_MDM_TIME=0 \
     persist.dbg.volte_avail_ovr=1 \
-    persist.volte_enabled_by_hw=1 \
-    DEVICE_PROVISIONED=1 \
-    ril.subscription.types=NV,RUIM
+    persist.volte_enabled_by_hw=1
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
